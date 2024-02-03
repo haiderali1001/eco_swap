4b7779534ad673fd43cfe8c7fe0ce1659d3e0747
haiderali1001 <haiderali.1001haider@gmail.com>
2024-02-03 12:02:14 +0530

    fixed deploy error import

--- Ecoswap/src/Cart.jsx
+++ Ecoswap/src/Cart.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import './Cart.css'
-import Header from './components/header';
+import Header from './components/Header';
 
 function Cart(){
   return (
