b5dc3859fe4d46ab424cae535ab884e94a6777b5
haiderali1001 <haiderali.1001haider@gmail.com>
2024-02-06 10:35:20 +0530

    shop frontend completed

--- Ecoswap/src/Shop.jsx
+++ Ecoswap/src/Shop.jsx
@@ -7,11 +7,11 @@ const baseURL = "https://mystiqueapi.onrender.com/products"
 // const baseURL = "http://localhost:3000/products"
 
 function Shop() {
-  const [post, setPost] = React.useState(null);
+  const [post, setPost] = React.useState([]);
 
   React.useEffect(() => {
     axios.get(baseURL).then((res) => {
-      console.log(res)
+      console.log(res);
       const productList = res.data;
       setPost(productList);
       post.map((ele) => {
@@ -25,17 +25,22 @@ function Shop() {
   return (
     <div className='shop'>
       <Header headerTitle={"Shop"} />
-
+      <div className="filter-bar">Sort functionality is being added</div>
       <div className='product-cards'>
 
         {
           post.map((ele) => {
             return (
-              <div className='card-pro'>
+              <div key={ele._id} className='card-pro'>
+                <div className='pro-overlay'>
+                  <button className='add-cart-btn'>Add to cart</button>
+                </div>
                 <img src={ele.image} />
-                <h4> {ele.title} </h4>
-                <h5>{ele.subcategory}</h5>
-                <h4>Rs.   {ele.price}</h4>
+                {/* <div className="dark-bg-card"> */}
+                  <h4> {ele.title} </h4>
+                  <h5>{ele.subcategory}</h5>
+                  <h4>Rs.   {ele.price}</h4>
+                {/* </div> */}
               </div>
             )
           })
